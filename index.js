b390e87548c5f7d9cd77c829558f97deaca50b52
weslen fabricio santos oliveira <weslenfabricioicloud.com@Papillon-2.local>
2023-12-31 12:04:11 +0100

    first git validation

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const prompt = require("prompt-sync")();
+const marcas = [
+  "nike",
+ "adidas",
+ "asics", 
+   "Es",
+ "balenciaga"
+];
+const novoArray = marcas.slice(3)
+console.log (novoArray);
+
